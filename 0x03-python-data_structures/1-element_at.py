ca1553fece63cbd1a26ff33f0d9582e1e369739d
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-09-26 16:50:20 +0000

    position getter

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,10 @@
 
 def element_at(my_list, idx):
     if idx > (len(my_list) - 1):
-        return None
+        return 'None'
 
     elif  idx < 0:
-        return None
+        return 'None'
 
     else:
         return (my_list[idx])
