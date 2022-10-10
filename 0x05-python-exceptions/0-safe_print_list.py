ca063ed6e0ad1a5bf25149ca2a4cebfeed13ec9a
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-10-10 18:56:24 +0000

    safely print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    num_printed = 0
+    list_type = type(my_list[0])
+    for x in range(x):
+        try:
+            print(list_type(my_list[x]), end='')
+            num_printed += 1
+
+        except IndexError:
+            break
+    print()
+    return num_printed
