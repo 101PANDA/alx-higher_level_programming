5dd7617e5611592a4e3365533cb9a3515848a235
FAVOUR OGIDI <1ogidifavour@gmail.com>
2022-10-10 19:01:25 +0000

    safely print list update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 
 def safe_print_list(my_list=[], x=0):
     num_printed = 0
-    list_type = type(my_list[0])
     for x in range(x):
         try:
+            list_type = type(my_list[x])
             print(list_type(my_list[x]), end='')
             num_printed += 1
 
