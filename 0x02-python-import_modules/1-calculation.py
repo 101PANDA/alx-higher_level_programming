a0cdf66425a0ca3bddef5b040a9500f50a6f5e6e
P_code <1ogidifavour@gmail.com>
2023-03-10 00:54:48 +0100

    solving requirements

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Import calculator_1 and run some calculation"""
 if __name__ == "__main__":
-    import calculator_1 as cal
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    suma = cal.add(a, b)
+    suma = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, suma))
-    diff = cal.sub(a, b)
+    diff = sub(a, b)
     print("{:d} - {:d} = {:d}".format(a, b, diff))
-    prod = cal.mul(a, b)
+    prod = mul(a, b)
     print("{:d} * {:d} = {:d}".format(a, b, prod))
-    divi = cal.div(a, b)
+    divi = div(a, b)
     print("{:d} / {:d} = {:d}".format(a, b, divi))
