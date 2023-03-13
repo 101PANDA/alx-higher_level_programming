053a913b4a7d4425fd903d9614dd5f70780b5486
P_code <1ogidifavour@gmail.com>
2023-03-13 22:00:36 +0100

    i guess we did -- resolved pycode issue

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Find multiples of of 2 in a list
    Assumes all input will always be a list containing int
-   return True or False depending on whether the  integer at the same position in the original list is a multiple of 2"""
+   return True or False depending on whether the
+   integer at the same position in the original list is a multiple of 2"""
+
 
 def divisible_by_2(my_list=[]):
     if my_list == []:
         return []
     result_list = []
     for x in my_list:
-        if x%2 == 0:
+        if x % 2 == 0:
             result_list.append(True)
         else:
             result_list.append(False)
