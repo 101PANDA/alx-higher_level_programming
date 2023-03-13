46bdb905fc7b1077aa8bd40afd06f636e437ed37
P_code <1ogidifavour@gmail.com>
2023-03-13 09:46:45 +0100

    pycode issue resolved

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     for x in my_string:
         if x == "C" or x == "c":
             my_string = my_string[:i] + my_string[(i+1):]
-            i-=1
-        i+=1
+            i -= 1
+        i += 1
     return my_string
